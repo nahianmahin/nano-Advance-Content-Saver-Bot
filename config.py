e25b4fb81344b787d8506310ec17185183cd9147
nahianmahin <158852699+nahianmahin@users.noreply.github.com>
2024-05-12 02:50:05 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = environ.get("API_ID", "")
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    BOT_SESSION = environ.get("BOT_SESSION", "") 
     DATABASE_URI = environ.get("DATABASE", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "nanosrcb")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
